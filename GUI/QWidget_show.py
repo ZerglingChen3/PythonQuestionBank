be006567a2bd62afa444cb4e8058bcbc0bf5594a
ZerglingChen3 <1908216413@qq.com>
2021-07-26 17:46:33 +0800

    finish a real login surface

--- GUI/QWidget_show.py
+++ GUI/QWidget_show.py
@@ -1,22 +1,52 @@
-from PyQt5.QtWidgets import (QWidget, QMessageBox, QDesktopWidget,
-                             QApplication, QGridLayout, QLabel,
-                             QSizePolicy, QLineEdit)
-from PyQt5.QtGui import QIcon
-from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
 
 
 class Example(QWidget):
 
     def __init__(self):
         super().__init__()
-        self.layout = QGridLayout()
+        self.main_widget = QWidget()
+        self.main_layout = QGridLayout()
+        self.main_layout.setSpacing(10)
+        self.main_widget.setLayout(self.main_layout)
+
+        self.login_surface_widget = QWidget()
+        self.login_surface_layout = QGridLayout()
+        self.login_surface_widget.setLayout(self.login_surface_layout)
+
+        self.login_button_widget = QWidget()
+        self.login_button_layout = QGridLayout()
+        self.login_button_widget.setLayout(self.login_button_layout)
+
         self.headLabel = QLabel("Python小信题库登录")
         self.unameLabel = QLabel("用户名：")
         self.pwdLabel = QLabel("  密码：")
-        self.unameText = QLineEdit()
-        self.pwdText = QLineEdit()
+
+        self.unameText = QLineEdit("admin")
+        self.pwdText = QLineEdit("password")
+
+        self.register_but = QPushButton("注册")
+        self.login_but = QPushButton("登录")
+
         self.initLabel()
+        # self.use_palette()
+        self.initPlace()
         self.initUI()
+
+    def initPlace(self):
+        self.main_layout.addWidget(self.headLabel, 0, 0, 9, 9)
+        self.main_layout.addWidget(self.login_surface_widget, 3, 3, 2, 3)
+        self.main_layout.addWidget(self.login_button_widget, 5, 3, 1, 3)
+        self.login_surface_layout.addWidget(self.unameLabel, 0, 0)
+        self.login_surface_layout.addWidget(self.unameText, 0, 1)
+        self.login_surface_layout.addWidget(self.pwdLabel, 1, 0)
+        self.login_surface_layout.addWidget(self.pwdText, 1, 1)
+        self.login_button_layout.addWidget(self.register_but, 0, 0)
+        self.login_button_layout.addWidget(self.login_but, 0, 1)
+
 
     def initLabel(self):
         self.headLabel.setAlignment(Qt.AlignHCenter)
@@ -26,22 +56,25 @@ class Example(QWidget):
                                      "font-family:Roman"
                                      "times;")
         self.headLabel.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        # self.main_layout.addWidget(self.headLabel, 0, 0, 4, 10)
 
         self.pwdText.setContextMenuPolicy(Qt.NoContextMenu)
 
-        self.unameText.setText("admin")
-        self.pwdText.setText("password")
-
-        self.layout.addWidget(self.headLabel, 0, 0, 5, 2)
-        self.layout.addWidget(self.unameLabel, 1, 0)
-        self.layout.addWidget(self.unameText, 1, 1)
-        self.layout.addWidget(self.pwdLabel, 2, 0)
-        self.layout.addWidget(self.pwdText, 2, 1)
+        '''
+        # self.layout.addWidget(self.none, 2, 0, 1, 4)
+        self.layout.addWidget(self.unameLabel, 2, 4, 1, 1)
+        self.layout.addWidget(self.unameText, 2, 5, 1, 1)
+        self.layout.addWidget(self.unameLabel, 2, 7, 1, 1)
+        # self.layout.addWidget(self.none, 2, 7, 1, 3)
+        self.layout.addWidget(self.pwdLabel, 3, 4, 1, 1)
+        self.layout.addWidget(self.pwdText, 3, 5, 1, 1)
+        self.layout.addWidget(self.register_but, 4, 4, 1, 1)
+        self.layout.addWidget(self.login_but, 4, 5, 1, 1)
+        '''
 
     def initUI(self):
-        self.layout.setSpacing(10)
-        self.setLayout(self.layout)
-        self.resize(2000, 1000)
+        self.setLayout(self.main_layout)
+        self.resize(1080, 960)
         self.center()
         self.setWindowTitle('小信题库')
         self.setWindowIcon(QIcon('./pictures/shixiaoxin.jpg'))
@@ -50,6 +83,7 @@ class Example(QWidget):
     """
     when close, remind people whether he want to leave
     """
+
     def closeEvent(self, event):
         reply = QMessageBox.question(self, '提醒',
                                      "你确定要退出吗？", QMessageBox.Yes |
@@ -63,6 +97,7 @@ class Example(QWidget):
     """
     set the windows into the middle
     """
+
     def center(self):
         qr = self.frameGeometry()
         cp = QDesktopWidget().availableGeometry().center()
@@ -76,3 +111,13 @@ class Example(QWidget):
     def keyPressEvent(self, e):
         if e.key() == Qt.Key_Escape:
             self.close()
+
+    def use_palette(self):
+        """
+        window_pale = QPalette()
+        pixmap = QBrush(QPixmap("./pictures/shixiaoxin.jpg").
+                        scaled(self.width(), self.height()))
+
+        window_pale.setBrush(QPalette.Background, QBrush(pixmap))
+        self.setPalette(window_pale)
+        """
